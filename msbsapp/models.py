54ba35e42cc739d92dfdc5bfa6baa52690be28ce
rakeshmadu <rakeshmadu24@gmail.com>
2019-08-01 12:16:14 +0530

    models not necessary

--- msbsapp/models.py
+++ msbsapp/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 class Data(models.Model):
     owner_id = models.CharField(max_length = 50)
     camapaign = models.CharField(max_length = 50)
